3b163b7eaefb1450ad01b022382678919670637d
Nik Kinkel <nskinkel@iastate.edu>
2015-09-14 01:12:49 -0500

    add Tm and Ipv4Addr parsing

--- src/directory/microstatusentry.rs
+++ src/directory/microstatusentry.rs
@@ -10,13 +10,13 @@ pub struct MicroStatusEntry {
     pub identity:       String,      /* a hash of the OR's identity key, encoded in
                                  base64, with the trailing equals sign(s)
                                  removed */
-    //publication:    Tm,       /* publication time of this OR's most recent
-    //                             descriptor, in UTC YYYY-MM-DD HH:MM:SS */
-    pub publication:      String,
+    pub publication:    Tm,       /* publication time of this OR's most recent
+                                 descriptor, in UTC YYYY-MM-DD HH:MM:SS */
+    //pub publication:      String,
 
     // TODO: can this be IPv6?
-    //ip:             Ipv4Addr, /* current IP address */
-    pub ip:             String,
+    pub ip:             Ipv4Addr, /* current IP address */
+    //pub ip:             String,
     pub orport:         u32,      /* current OR port */
     pub dirport:        u32,      /* current directory port, or 0 for none */
 
@@ -32,7 +32,6 @@ mod tests {
     #[test]
     fn minimal_micro_status_entry() {
         let s = "r PDrelay1 AAFJ5u9xAqrKlpDW6N0pMhJLlKs 2015-09-11 04:57:25 95.215.44.189 8080 0\nm 3zNMfCiqAaDtvak3divNYzeYxh/f9OiCBcs6YKU1RrU\ns Fast Running Stable Valid\nv Tor 0.2.7.2-alpha-dev\nw Bandwidth=549\n";
-        //assert_eq!(micro_status_entry(s), Ok(()));
         println!("{:?}", micro_status_entry(s));
     }
 }
