e7ba10351c44384d63d3bbef41ec08be42fba5bb
Nik Kinkel <nskinkel@iastate.edu>
2015-09-14 20:02:28 -0500

    improve micro status entry unit test

--- src/directory/microstatus.rs
+++ src/directory/microstatus.rs
@@ -2,7 +2,7 @@ use std::collections::{HashMap};
 use std::net::{Ipv4Addr, Ipv6Addr};
 use time::{Tm};
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub enum Flag {
     Authority,
     BadExit,
@@ -17,13 +17,13 @@ pub enum Flag {
     V2Dir,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct VotingDelay {
     vote_seconds:   u32,
     dist_seconds:   u32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct Preamble {
     version:            u32,
     method:             u32,
@@ -38,45 +38,71 @@ pub struct Preamble {
     params:             Option<HashMap<String, u32>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct Authority;
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct Entry {
-    /* 'r' line */
     pub nickname:       String,
     pub identity:       String,
+    // TODO: use something other than Tm here?
     pub publication:    Tm,
-    // XXX: can this ever be IPv6?
+    // TODO: can this ever be IPv6?
     pub ip:             Ipv4Addr,
     pub orport:         u32,
     pub dirport:        u32,
-
-    /* 'm' line */
     pub digest:         String,
-
-    /* 's' line */
     pub flags:          Vec<Flag>,
-
-    /* 'v' line */
     pub version:        Option<String>,
-
-    /* 'w' line */
     pub bandwidth:      Option<u32>,
     pub unmeasured:     Option<bool>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct Footer;
 
 #[cfg(test)]
 mod tests {
+    use std::net::{Ipv4Addr};
+    use time::{Tm};
+
     use directory::directory_grammar::{micro_status_entry, micro_status_preamble};
+    use super::*;
     
     #[test]
     fn minimal_micro_status_entry() {
         let s = "r PDrelay1 AAFJ5u9xAqrKlpDW6N0pMhJLlKs 2015-09-11 04:57:25 95.215.44.189 8080 0\nm 3zNMfCiqAaDtvak3divNYzeYxh/f9OiCBcs6YKU1RrU\ns Fast Running Stable Valid\nv Tor 0.2.7.2-alpha-dev\nw Bandwidth=549\n";
-        println!("{:?}", micro_status_entry(s));
+
+        let expected = Entry {
+            nickname:       "PDrelay1".to_string(),
+            identity:       "AAFJ5u9xAqrKlpDW6N0pMhJLlKs".to_string(),
+            publication:    Tm {
+                                    tm_year:    115,
+                                    tm_mon:     8,
+                                    tm_mday:    11,
+                                    tm_hour:    4,
+                                    tm_min:     57,
+                                    tm_sec:     25,
+                                    tm_wday:    0,
+                                    tm_yday:    0,
+                                    tm_isdst:   0,
+                                    tm_utcoff:  0,
+                                    tm_nsec:    0,
+                                },
+            ip:             "95.215.44.189".parse().unwrap(),
+            orport:         8080,
+            dirport:        0,
+            digest:         "3zNMfCiqAaDtvak3divNYzeYxh/f9OiCBcs6YKU1RrU"
+                                .to_string(),
+            flags:          vec![Flag::Fast, Flag::Running, Flag::Stable,
+                                 Flag::Valid],
+            version:        Some("Tor 0.2.7.2-alpha-dev".to_string()),
+            bandwidth:      Some(549),
+            unmeasured:     None,
+        };
+
+        let result = micro_status_entry(s).ok().expect("parsing failed!");
+        assert_eq!(result, expected);
     }
 
     #[test]
